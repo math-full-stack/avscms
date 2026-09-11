8a05a6f4c65533e6aed39b18099fbd56f13cf9bd
math <math@xbrasil>
2026-09-11 20:22:59 -0300

    up

--- sql/migration_grabber_settings.sql
+++ sql/migration_grabber_settings.sql
@@ -3,7 +3,7 @@
 
 CREATE TABLE IF NOT EXISTS `grabber_settings` (
   `setting_key` varchar(100) NOT NULL DEFAULT '',
-  `setting_value` text NOT NULL DEFAULT '',
+  `setting_value` text NOT NULL,
   `updated_at` int(11) unsigned NOT NULL DEFAULT 0,
   PRIMARY KEY (`setting_key`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
